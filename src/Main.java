08c2edbff2881a25810bd180c13daae3f192e9cc
Elkhan <is.elxan@gmail.com>
2021-04-05 03:15:48 +0400

    feature: add basic login page view & change structure

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
 
