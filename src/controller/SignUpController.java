7438fb5a81c0ce9ee85fbc12727ca6754d578289
Elkhan <is.elxan@gmail.com>
2021-06-06 04:57:03 +0400

    feature: add signUp addItem & modify login

---
+++ src/controller/SignUpController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+    public TextField signUpFirstName;
+    public TextField signUpLastName;
+    public CheckBox signUpCheckBoxFemale;
+    public TextField signUpUsername;
+    public TextField signUpLocation;
+    public CheckBox signUpCheckBoxMale;
+    public TextField signUpPassword;
+    public Button signUpButton;
+
+    @FXML
+    void initialize() {
+
+    }
+}
